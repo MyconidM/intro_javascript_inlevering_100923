b75412309ed29389e04e039a1647d6be4dc0a40f
MyconidM <111729616+MyconidM@users.noreply.github.com>
2023-09-08 22:10:14 +0200

    liste

--- Oppgave_04/src/ingen_hjelp.js
+++ Oppgave_04/src/ingen_hjelp.js
@@ -1,16 +1,35 @@
-import './styles.css';
+//import './styles.css';
 
 // TODO: Bruk getElementById til å hente HTML-elementene med #id
 const guess = document.getElementById("guess");
 const numbers = document.getElementById("numbers");
-const btn = document.getElementById("button");
 
 
 // TODO: Bruk querySelector til å hente knappen
+const btn = document.querySelector("button");
+
+
 // TODO: Lag en liste med tallene som skal sorteres
+const randomeNumber = () => {
+    return Math.floor(Math.random() * 100);
+}
+
+ let NumberList = [] ;
+const listGenerator = () => {
+    for (let i = 0; i < 5; i++) {
+        NumberList.push(randomeNumber());
+    }
+}
+listGenerator()
+console.log(NumberList)
+
 // TODO: Lag en funksjon for å skrive ut tallene som skal sorteres
+
 // TODO: Lag en funksjon for å skrive ut input felter bruker kan skrive tallene i
 // TODO: Lag en funksjon for å hente ut det brukeren har skrevet inn
 // TODO: Lag en funksjon for å sjekke om tallene er sortert riktig av bruker
 // TODO: Lag en funksjon som "lager UI" --Nødvendig?
 // TODO: Lytt til 'click'-event og trigg checkNumberSeq ved klikk
+btn.addEventListener("click", () => {
+    //if(SortedList === guess) {congrets resttliste med delay}
+})
